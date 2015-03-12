449d5f59ee4f6739f8b908befc1bf81054dd28fb
Annie <anne.bourgeois1@gmail.com>
2015-03-12 15:52:17 -0400

    Fixed default avatar suffix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
                     styles: { :medium => "300x300>", :thumb => "100x100>" },
-                    default_url: 'missing_avatar.jpg'
-  #default image is stored in app/assets/images/medium/missing_avatar.jpg
+                    default_url: 'missing_avatar.png'
+  #default image is stored in app/assets/images/medium/missing_avatar.png
 end
