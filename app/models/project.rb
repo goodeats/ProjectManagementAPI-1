85ae227adc26d0f3b601b875b3a856e46e04092b
KBondanza <kelly.bondanza@gmail.com>
2015-03-12 15:31:29 -0400

    update models with dependent destroy and added a comment model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  belongs_to :group
+  belongs_to :group, dependent: :destroy
   has_many :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
   belongs_to :user
-  belongs_to :project
+  belongs_to :project, dependent: :destroy
 end
